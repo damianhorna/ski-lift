150f550dc79d624c2770b60655b381fa6d96a190
damian-horna <damian.horna@interia.pl>
2019-04-17 16:31:23 +0200

    Made queue more generic

--- ski.c
+++ ski.c
@@ -3,71 +3,53 @@
 #include <stdlib.h> 
 #include <limits.h> 
 
+struct QueueElement
+{
+	int id;
+	int time;
+	int weight;
+	struct QueueElement *previous;
+	struct QueueElement *next;
+} QueueElement;
+
 struct Queue 
 { 
-    int front, rear, size; 
-    unsigned capacity; 
-    int* array; 
-}; 
-  
-// function to create a queue of given capacity.  
-// It initializes size of queue as 0 
-struct Queue* createQueue(unsigned capacity) 
+	struct QueueElement *head;
+	int size;
+}Queue; 
+
+struct Queue* createQueue(struct QueueElement* firstElement) 
 { 
-    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue)); 
-    queue->capacity = capacity; 
-    queue->front = queue->size = 0;  
-    queue->rear = capacity - 1;  // This is important, see the enqueue 
-    queue->array = (int*) malloc(queue->capacity * sizeof(int)); 
-    return queue; 
+	struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue)); 
+	queue->head = firstElement; 
+	queue->size = 1;  
+	return queue; 
 } 
   
-// Queue is full when size becomes equal to the capacity  
-int isFull(struct Queue* queue) 
-{  return (queue->size == queue->capacity);  } 
-  
-// Queue is empty when size is 0 
 int isEmpty(struct Queue* queue) 
 {  return (queue->size == 0); } 
   
-// Function to add an item to the queue.   
-// It changes rear and size 
-void enqueue(struct Queue* queue, int item) 
+void enqueue(struct Queue* queue, struct QueueElement* queueElement) 
 { 
-    if (isFull(queue)) 
-        return; 
-    queue->rear = (queue->rear + 1)%queue->capacity; 
-    queue->array[queue->rear] = item; 
-    queue->size = queue->size + 1; 
-    printf("%d enqueued to queue\n", item); 
+	struct QueueElement *elem = queue->head;
+	while(elem->next != NULL)
+	{
+		elem = elem->next;
+	}
+	elem->next = queueElement;
+	queue->size = queue->size + 1; 
+	printf("element enqueued to queue\n"); 
 } 
   
-// Function to remove an item from queue.  
-// It changes front and size 
-int dequeue(struct Queue* queue) 
+struct QueueElement* dequeue(struct Queue* queue) 
 { 
-    if (isEmpty(queue)) 
-        return INT_MIN; 
-    int item = queue->array[queue->front]; 
-    queue->front = (queue->front + 1)%queue->capacity; 
-    queue->size = queue->size - 1; 
-    return item; 
-} 
-  
-// Function to get front of queue 
-int front(struct Queue* queue) 
-{ 
-    if (isEmpty(queue)) 
-        return INT_MIN; 
-    return queue->array[queue->front]; 
-} 
-  
-// Function to get rear of queue 
-int rear(struct Queue* queue) 
-{ 
-    if (isEmpty(queue)) 
-        return INT_MIN; 
-    return queue->array[queue->rear]; 
+	if (isEmpty(queue)) 
+		return NULL; 
+
+	struct QueueElement* item = queue->head;
+	queue->head = item->next;
+	queue->size = queue->size - 1; 
+	return item; 
 } 
 
 int main( int argc, char **argv )
@@ -75,6 +57,7 @@ int main( int argc, char **argv )
 	int rank, size;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int namelen;
+
 
 	MPI_Init( &argc, &argv );
 	
