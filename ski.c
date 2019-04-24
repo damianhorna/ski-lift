21fa39a033a8f7de48d37b3f89a44822bf1f3561
Kamil <Kamil.plucinski97@gmail.com>
2019-04-24 16:32:26 +0200

    queue done

--- ski.c
+++ ski.c
@@ -1,4 +1,4 @@
-#include "mpi.h"
+// #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h> 
 #include <limits.h> 
@@ -31,14 +31,20 @@ int isEmpty(Queue* queue)
   
 void enqueue(Queue* queue, QueueElement* queueElement) 
 { 
-	QueueElement *elem = queue->head;
-	while(elem->next != NULL)
-	{
-		elem = elem->next;
+	if(queue->size == 0){
+		queue->head = queueElement; 
+		queue->size = 1;  
+	} else {
+		QueueElement *elem = queue->head;
+		while(elem->next != NULL)
+		{
+			elem = elem->next;
+		}
+		elem->next = queueElement;
+		queue->size = queue->size + 1; 
+		printf("element enqueued to queue\n"); 
 	}
-	elem->next = queueElement;
-	queue->size = queue->size + 1; 
-	printf("element enqueued to queue\n"); 
+	
 } 
   
 struct QueueElement* dequeue(Queue* queue) 
@@ -50,21 +56,40 @@ struct QueueElement* dequeue(Queue* queue)
 	queue->head = item->next;
 	queue->size = queue->size - 1; 
 	return item; 
-} 
+}
+
 
 int main( int argc, char **argv )
 {
+
+	printf("main\n");
 	int rank, size;
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	// char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int namelen;
+	QueueElement *element = (QueueElement*) malloc(sizeof(QueueElement));
+	Queue* queue = createQueue(element);
+	printf("after create%d\n", isEmpty(queue));
+
+	QueueElement *element2 = (QueueElement*) malloc(sizeof(QueueElement));
+	enqueue(queue, element2);
+	printf("size: %d\n", queue->size);
+	dequeue(queue);
+	printf("size: %d\n", queue->size);
+
+	dequeue(queue);
+	printf("size: %d\n", queue->size);
+
+	QueueElement *element3 = (QueueElement*) malloc(sizeof(QueueElement));
+	enqueue(queue, element3);
+	printf("size: %d\n", queue->size);
 
 
-	MPI_Init( &argc, &argv );
+	// MPI_Init( &argc, &argv );
 	
-	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-	MPI_Comm_size( MPI_COMM_WORLD, &size );
-	MPI_Get_processor_name(processor_name,&namelen);
-	printf( "Jestem %d z %d na %s\n", rank, size, processor_name );
+	// MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	// MPI_Comm_size( MPI_COMM_WORLD, &size );
+	// MPI_Get_processor_name(processor_name,&namelen);
+	// printf( "Jestem %d z %d na %s\n", rank, size, processor_name );
 
-	MPI_Finalize();
+	// MPI_Finalize();
 }
